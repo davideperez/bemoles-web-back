6343b90e77c7a76229bf9b492b45500488b89021
emiliano alfaraz <emialfa2@gmail.com>
2023-06-09 00:41:45 -0300

    add update event controller

--- src/models/events/events.model.js
+++ src/models/events/events.model.js
@@ -15,14 +15,14 @@ const eventsDataBase = require('./events.mongo')
 const event = {
     title: "Concierto de Piano en Burzaco 3",
     date: new Date('May 25, 2023'),
-    flyer: "flyer.jpg",
+    image: "flyer.jpg",
     info: "Concierto a cargo del pianista Pierre Aimard, obras de Messiaen.",
     price: 1500,
     maxAttendance: 800,
     paymentLink: 'https://www.mercadopago.com.ar',
 }
 
-saveEventInMongoDB(event)
+// upsertEventByIdInMongoDB(event);
 
 //-----------------------------------------------------------------------------------------------------//
 // Behaviours //
@@ -55,8 +55,37 @@ async function getEvent (eventId) {
     }
 }
 
+async function createEventByIdInMongoDB (event) {
+    try {
+        const newEvent = new eventsDataBase(event);
+        return await newEvent.save();
+    } catch (err) {
+        console.error(`No se pudo crear el evento en la base de datos: ${err}`)
+    }
+} 
+
+async function updateEventByIdInMongoDB (eventId, event) {
+    try {
+        await eventsDataBase.findByIdAndUpdate(eventId, event, { new: true }) 
+    } catch (err) {
+        console.error(`No se pudo actualziar el evento en la base de datos: ${err}`)
+    }
+} 
+
+async function upsertEventByIdInMongoDB (event) { //TODO
+    try {
+        await eventsDataBase.updateOne({ //TODO validar si la que conviene es events.create() o events.updateOne()
+            _id: event._id, 
+        }, event, {
+            upsert: true,
+        }) 
+    } catch (err) {
+        console.error(`No se pudo salvar el event en la base de datos: ${err}`)
+    }
+} 
+
 // agrega o actualiza el event a Mongo DB Atlas.
-async function saveEventInMongoDB (event) { //TODO
+async function saveEventByTitleInMongoDB(event) { //TODO
     try {
         await eventsDataBase.updateOne({ //TODO validar si la que conviene es events.create() o events.updateOne()
             title: event.title, 
@@ -80,6 +109,9 @@ async function deleteEventById (eventId) { //TODO
 module.exports = {
     getAllEvents,
     getEvent,
-    saveEventInMongoDB,
+    createEventByIdInMongoDB,
+    updateEventByIdInMongoDB,
+    upsertEventByIdInMongoDB,
+    saveEventByTitleInMongoDB,
     deleteEventById
 }
