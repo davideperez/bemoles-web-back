43ac8406494fdd6359c6c5246b9cc14925b22d59
Emiliano M Alfaraz <71176986+emialfa@users.noreply.github.com>
2023-11-10 20:14:30 -0300

    Adding function to convert datetime to the Argentina timezone and implement in emails

--- src/helpers/convertDatetimeToTimezone.js
+++ src/helpers/convertDatetimeToTimezone.js
@@ -1,7 +1,7 @@
 const formatDatetimeToTimezone = (date) => {
     const newDate = new Date(date);
     const localOffset = newDate.getTimezoneOffset() * 60000;
-    const offsetFromArgentina = 3 * 60 * 60 * 1000;
+    const offsetFromArgentina = -3 * 60 * 60 * 1000;
     const adjustedDate = new Date(newDate.getTime() - localOffset + offsetFromArgentina);
     return adjustedDate;
   }
